4266f0c7c06526b4bca3c4531786d04201894d6d
nguyenhuuton123 <nhtonx1@gmail.com>
2024-01-22 17:14:29 +0700

    Create category, product tables db script

---
+++ src/main/resources/db/20240122_Create_Category_Product_Tables.sql
@@ -0,0 +1,37 @@
+create table if not exists category
+(
+    id   int         not null auto_increment,
+    name varchar(50) not null,
+    constraint category_pk primary key (id)
+);
+
+create table if not exists size
+(
+    id         int not null auto_increment,
+    name       varchar(100),
+    product_id int,
+    constraint size_pk primary key (id),
+    constraint size_product_fk foreign key (product_id) references product (id)
+);
+
+create table if not exists color
+(
+    id         int not null auto_increment,
+    name       varchar(100),
+    product_id int,
+    constraint color_pk primary key (id),
+    constraint color_product_fk foreign key (product_id) references product (id)
+);
+
+create table if not exists product
+(
+    id          int not null auto_increment,
+    category_id int,
+    name        varchar(100),
+    price       double,
+    image       varchar(200),
+    description varchar(100),
+    quantity    int,
+    constraint product_pk primary key (id),
+    constraint product_category_fk foreign key (category_id) references category (id)
+);
