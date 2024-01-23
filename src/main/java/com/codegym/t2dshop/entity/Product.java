b9bdcd9b42570fea7cec1066dedb29b4e6fe778f
nguyenhuuton123 <nhtonx1@gmail.com>
2024-01-23 17:42:07 +0700

    entity Color Product Size

---
+++ src/main/java/com/codegym/t2dshop/entity/Product.java
@@ -0,0 +1,33 @@
+package com.codegym.t2dshop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer price;
+    private String description;
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+    @OneToMany(mappedBy = "product")
+    private List<Size> sizes = new ArrayList<>();
+    @OneToMany(mappedBy = "product")
+    private List<Color> colors = new ArrayList<>();
+}
---
+++ src/main/java/com/codegym/t2dshop/entity/Size.java
@@ -0,0 +1,27 @@
+package com.codegym.t2dshop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.parameters.P;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Size {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
